aad1298976a8333389ad7279d0927fd8499d0f11
Taylor Crowe <tcrowe14@shaw.ca>
2024-09-26 17:43:36 -0600

    Updated buttons
    tweaked the disabled button press to flash grey instead of whatever color the button is when it disabled

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -8,17 +8,17 @@ export default function NewItem() {
     const decrement = () => setQuantity(quantity - 1);
 
     return (
-        <div class="flex bg-red-100 w-48 h-10">
+        <div className="flex bg-red-100 w-48 h-10">
             <div className="flex w-24 justify-center items-center font-bold">
                 {quantity}
             </div>
             <div className="flex w-12 justify-center items-center pl-2 pr-1">
-                <button className="w-12 bg-red-600 hover:bg-red-500 disabled:bg-gray-400 font-bold rounded-md active:outline-none active:ring-2 active:ring-red-400" onClick={decrement} disabled={quantity == 1}>
+                <button className="w-12 bg-red-600 hover:bg-red-500 disabled:bg-gray-400 font-bold rounded-md active:outline-none active:ring-2 active:ring-red-400 disabled:active:ring-gray-300" onClick={decrement} disabled={quantity == 1}>
                     -
                 </button>
             </div>
             <div className="flex w-12 justify-center items-center pr-2 pl-1">
-                <button className="w-12 bg-green-600 hover:bg-green-500 disabled:bg-gray-400 font-bold rounded-md active:outline-none active:ring-2 active:ring-green-400" onClick={increment} disabled={quantity == 20}>
+                <button className="w-12 bg-green-600 hover:bg-green-500 disabled:bg-gray-400 font-bold rounded-md active:outline-none active:ring-2 active:ring-green-400 disabled:active:ring-gray-300" onClick={increment} disabled={quantity == 20}>
                     +
             </button>
             </div>
