ef1bb602b9a21b1577da62387da6a3be2d44df7b
Taylor Crowe <tcrowe14@shaw.ca>
2024-11-01 08:10:17 -0600

    Updated shopping list
    added a redirect after logging in and a button to navigate back to the index page.

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -2,39 +2,46 @@
 
 import { useUserAuth } from "./_utils/auth-context";
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
     const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+    const router = useRouter();
 
+    const home = async () => {
+        router.push("/");
+    };
+    
     const login = async () => {
         await gitHubSignIn();
+        router.push("/week-9/shopping-list");
     };
 
     const logout = async () => {
         await firebaseSignOut();
     };
-    
-    // Check if the user is not logged in, display a message
+
     if (user === null) {
         return (
             <main>
-                <h1>Week 9 Shopping List App</h1>
-                <button onClick={login}>Login with GitHub</button>
-                <p>Please log in to see the shopping list</p>
+                <h1 className="text-3xl font-bold m-2">Shopping List App</h1>
+                <p className="m-2">Please log in to see the shopping list</p>
+                <button className="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded ml-6 mt-3" onClick={login}>Login with GitHub</button>
+                <button className="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded ml-6 mt-3" onClick={home}>Index</button>
             </main>
         );
     }
 
-    // If the user is logged in, display the shopping list link and logout button
     return (
         <main>
-            <h1>Week 9 Shopping List App</h1>
-            <button onClick={logout}>Logout</button>
+            <h1 className="text-3xl font-bold m-2">Shopping List App</h1>
             <div>
-            <Link href="/week-9/shopping-list">
-                Go to Shopping List
-            </Link>
+                <Link href="/week-9/shopping-list" className="m-2">
+                    Go to Shopping List
+                </Link>
             </div>
+            <button className="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded ml-6 mt-3" onClick={logout}>Logout</button>
+            <button className="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded ml-6 mt-3" onClick={home}>Index</button>
         </main>
     );
 }
--- app/week-9/shopping-list/page.js
+++ app/week-9/shopping-list/page.js
@@ -5,16 +5,22 @@ import NewItem from "./new-item";
 import itemData from "./items.json";
 import { useState } from "react";
 import { useUserAuth } from "../_utils/auth-context";
+import { useRouter } from "next/navigation";
 
 
 export default function Page() {
     const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
+    const router = useRouter();
     const [items, setItems] = useState(itemData);
     const [selectedItemName, setSelectedItemName] = useState('');
     const [sortBy, setSortBy] = useState("name");
     const [categoryEnabled, setCategoryEnabled] = useState(true);
     const [nameEnabled, setNameEnabled] = useState(true);
+
+    const home = async () => {
+        router.push("/");
+    };
 
     function handleSortChange() {
         if(sortBy === 'category') {
@@ -51,9 +57,10 @@ export default function Page() {
     if (!user) {
         return (
             <main>
-                <h1>Week 9 Shopping List App</h1>
-                <button onClick={gitHubSignIn}>Login with GitHub</button>
-                <p>Please log in to see the shopping list</p>
+                <h1 className="text-3xl font-bold m-2">Shopping List App</h1>
+                <p className="m-2">Please log in to see the shopping list</p>
+                <button className="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded ml-6 mt-3" onClick={gitHubSignIn}>Login with GitHub</button>
+                <button className="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded ml-6 mt-3" onClick={home}>Index</button>
             </main>
         );
     }
@@ -83,7 +90,8 @@ export default function Page() {
                 <section className="flex">
                     <ItemList items={items} onItemSelect={handleItemSelect}/>
                 </section>
-                <button onClick={firebaseSignOut}>Logout</button>
+                <button className="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded ml-6 mt-3" onClick={firebaseSignOut}>Logout</button>
+                <button className="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded ml-6 mt-3" onClick={home}>Index</button>
                 </div>
 
                 <div className="w-2/3 ml-3 max-h-full overflow-y-auto">
