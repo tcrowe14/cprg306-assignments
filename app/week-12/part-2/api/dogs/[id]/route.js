37c005b0dc8793fdaa1353eb467f1760a75b6e6d
Taylor Crowe <tcrowe14@shaw.ca>
2024-11-21 16:57:57 -0700

    adjusted week-12
    had to comment out all of week-12 as the port 3000 was hard coded and wouldn't deploy in vercel.

--- app/week-12/part-2/api/dogs/[id]/route.js
+++ app/week-12/part-2/api/dogs/[id]/route.js
@@ -1,65 +1,65 @@
-// fetch one dog
-// GET /api/dogs/:id
-export async function GET(request, { params }) {
-    const { id } = await params; // params is a promise
-    const idNum = Number(id); // all params are strings
+// // fetch one dog
+// // GET /api/dogs/:id
+// export async function GET(request, { params }) {
+//     const { id } = await params; // params is a promise
+//     const idNum = Number(id); // all params are strings
   
-    // SELECT * FROM dogs where id=${idNum}`;
-    const dogs = [
-      { id: 1, name: "Luna", age: 2 },
-      { id: 2, name: "Max", age: 4 },
-    ];
-    const dog = dogs.find((dog) => dog.id === idNum);
+//     // SELECT * FROM dogs where id=${idNum}`;
+//     const dogs = [
+//       { id: 1, name: "Luna", age: 2 },
+//       { id: 2, name: "Max", age: 4 },
+//     ];
+//     const dog = dogs.find((dog) => dog.id === idNum);
   
-    if (!dog) {
-      return new Response("wrong id", { status: 404 });
-    }
+//     if (!dog) {
+//       return new Response("wrong id", { status: 404 });
+//     }
   
-    console.log(`fetch dog ${id}`);
-    return new Response(JSON.stringify(dog), { status: 200 });
-  }
+//     console.log(`fetch dog ${id}`);
+//     return new Response(JSON.stringify(dog), { status: 200 });
+//   }
   
-  // update all the information of a dog
-  export async function PUT(request, { params }) {
-    const { id } = await params;
-    const idNum = Number(id);
-    const dog = await request.json();
+//   // update all the information of a dog
+//   export async function PUT(request, { params }) {
+//     const { id } = await params;
+//     const idNum = Number(id);
+//     const dog = await request.json();
   
-    // update dog in database
-    // UPDATE dogs SET name = ${dog.name}, age = ${dog.age} WHERE id = ${idNum}
+//     // update dog in database
+//     // UPDATE dogs SET name = ${dog.name}, age = ${dog.age} WHERE id = ${idNum}
   
-    console.log(`(full) update dog ${idNum} with ${JSON.stringify(dog)}`);
-    return new Response(null, { status: 204 });
-  }
+//     console.log(`(full) update dog ${idNum} with ${JSON.stringify(dog)}`);
+//     return new Response(null, { status: 204 });
+//   }
   
-  // update partial information of a dog
-  export async function PATCH(request, { params }) {
-    const { id } = await params;
-    const idNum = Number(id);
-    const dog = await request.json();
+//   // update partial information of a dog
+//   export async function PATCH(request, { params }) {
+//     const { id } = await params;
+//     const idNum = Number(id);
+//     const dog = await request.json();
   
-    if (dog.name) {
-      // update name in database
-      // UPDATE dogs SET name = ${dog.name} WHERE id = ${idNum}
-    }
+//     if (dog.name) {
+//       // update name in database
+//       // UPDATE dogs SET name = ${dog.name} WHERE id = ${idNum}
+//     }
   
-    if (dog.age) {
-      // update age in database
-      // UPDATE dogs SET age = ${dog.age} WHERE id = ${idNum}
-    }
+//     if (dog.age) {
+//       // update age in database
+//       // UPDATE dogs SET age = ${dog.age} WHERE id = ${idNum}
+//     }
   
-    console.log(`(partial) update dog ${idNum} with ${JSON.stringify(dog)}`);
-    return new Response(null, { status: 204 });
-  }
+//     console.log(`(partial) update dog ${idNum} with ${JSON.stringify(dog)}`);
+//     return new Response(null, { status: 204 });
+//   }
   
-  // delete a dog
-  export async function DELETE(request, { params }) {
-    const { id } = await params;
-    const idNum = Number(id);
+//   // delete a dog
+//   export async function DELETE(request, { params }) {
+//     const { id } = await params;
+//     const idNum = Number(id);
   
-    // delete dog in database
-    // DELETE FROM dogs WHERE id = ${idNum}
+//     // delete dog in database
+//     // DELETE FROM dogs WHERE id = ${idNum}
   
-    console.log(`delete dog ${idNum}`);
-    return new Response(null, { status: 204 });
-  }
+//     console.log(`delete dog ${idNum}`);
+//     return new Response(null, { status: 204 });
+//   }
--- app/week-12/part-3/api/route.js
+++ app/week-12/part-3/api/route.js
@@ -1,31 +1,31 @@
-// Problem: we don't know the shape of the request body
+// // Problem: we don't know the shape of the request body
 
-// Solution: check the request body
+// // Solution: check the request body
 
-// create a new dog
-export async function POST(request) {
-    const newDogData = await request.json();
+// // create a new dog
+// export async function POST(request) {
+//     const newDogData = await request.json();
   
-    // validate the request body
-    if (
-      !newDogData.name ||
-      !newDogData.age ||
-      newDogData.name === "" ||
-      newDogData.age === "" ||
-      isNaN(newDogData.age) ||
-      newDogData.age < 0
-    ) {
-      return new Response("Invalid request body", { status: 400 });
-    }
+//     // validate the request body
+//     if (
+//       !newDogData.name ||
+//       !newDogData.age ||
+//       newDogData.name === "" ||
+//       newDogData.age === "" ||
+//       isNaN(newDogData.age) ||
+//       newDogData.age < 0
+//     ) {
+//       return new Response("Invalid request body", { status: 400 });
+//     }
   
-    const newDog = {
-      name: newDogData.name,
-      age: newDogData.age,
-    };
+//     const newDog = {
+//       name: newDogData.name,
+//       age: newDogData.age,
+//     };
   
-    // create new dog in database
-    // ...
+//     // create new dog in database
+//     // ...
   
-    // return the new dog
-    return new Response(JSON.stringify(newDog), { status: 201 });
-  }
+//     // return the new dog
+//     return new Response(JSON.stringify(newDog), { status: 201 });
+//   }
